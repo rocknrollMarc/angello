55b2b761b3b3883569160a0f758f011bfcafbda4
Marc <marc.bluemner@gmail.com>
2014-09-26 09:18:22 +0200

    index story form

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ myModule.controller('MainCtrl', function($scope) {
   };
 
   $scope.stories = [
-    {title: 'Story 00', description: 'Description pending.'},
+    {title: 'Story 00', description: 'Description pending.', criteria: 'Criteria pending.', status: 'To Do', type: 'Feature', reporter: 'Lukas Ruebbelke', assignee: 'Brian Ford'},
     {title: 'Story 01', description: 'Description pending.'},
     {title: 'Story 02', description: 'Description pending.'},
     {title: 'Story 03', description: 'Description pending.'},
