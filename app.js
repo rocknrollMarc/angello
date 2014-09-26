f44bbe60e4bcade23d3b32fc8e42a4d5dbf542fe
Marc <marc.bluemner@gmail.com>
2014-09-26 08:45:12 +0200

    main controller

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 var myModule = angular.module('Angello', []);
-myModule.controller('MainCtrl', function($scope) { });
+myModule.controller('MainCtrl', function($scope) {
+  $scope.currentStory;
+
+  $scope.setCurrentStory = function(story) {
+    $scope.currentStory = story;
+  };
+
+  $scope.stories = [
+    {title: 'Story 00', description: 'Description pending.'},
+    {title: 'Story 01', description: 'Description pending.'},
+    {title: 'Story 02', description: 'Description pending.'},
+    {title: 'Story 03', description: 'Description pending.'},
+    {title: 'Story 04', description: 'Description pending.'},
+    {title: 'Story 05', description: 'Description pending.'}
+  ];
+});
